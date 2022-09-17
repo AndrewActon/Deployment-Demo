4828ee0786b22fca44aa0fb6086a3346978e15e3
Andrew Acton <andrew.acton.aa@gmail.com>
2022-09-17 17:13:10 -0600

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+// Heroku makes the env. This line uses the env port OR a default 4005.
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
