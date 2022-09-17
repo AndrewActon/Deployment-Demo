29543c25024b1f83d5b7ed7d04781b425fc0fe05
Andrew Acton <andrew.acton.aa@gmail.com>
2022-09-17 17:43:59 -0600

    change to middleware from endpoints

--- server/server.js
+++ server/server.js
@@ -6,15 +6,27 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Only need middleware or endpoints for Heroku to work.
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+// *** Middleware ***
 
-//Importing CSS file for front end. Must be exact same string from index.html link href.
-app.get('/styles', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.css'))
-})
+app.use(express.static(path.join(__dirname, '../public')))
+
+// *** Endpoints ***
+
+// Can send entire folder to Heroku for Heroku to be able to serve up everything. 
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public'))
+// })
+
+// app.get('/', function(req, res){
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
+
+// //Importing CSS file for front end. Must be exact same string from index.html link href.
+// app.get('/styles', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.css'))
+// })
 
 // Heroku makes the env. This line uses the env port OR a default 4005.
 const port = process.env.PORT || 4005
